212eb746c77f2d1e4ddda9c6487fbd52ef97b94d
Nguyen Tien Toan <nguyentoan31072k3@gmail.com>
2023-12-13 16:01:29 +0700

    adding base entity. closes #10

---
+++ pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.thomas.sfgpetclinic.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
