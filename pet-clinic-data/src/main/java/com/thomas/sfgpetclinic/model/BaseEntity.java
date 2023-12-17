369a42cdf33e80604917d837a483b50126557619
Nguyen Tien Toan <nguyentoan31072k3@gmail.com>
2023-12-17 17:45:23 +0700

    Updated Base Entity with JPA annotations . Closes #35

--- pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/model/BaseEntity.java
@@ -3,9 +3,11 @@ package com.thomas.sfgpetclinic.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
