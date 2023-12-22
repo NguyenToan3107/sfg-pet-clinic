04d45a5063b425c6cabdfa34b5d16e9b16c51da8
Toàn Nguyễn <nguyentoan31072k3@gmail.com>
2023-12-22 12:09:01 +0700

    Implemented Find Owners. Closes #54

--- pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.thomas.sfgpetclinic.repositories;
 import com.thomas.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/thomas/sfgpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.thomas.sfgpetclinic.services;
 
 import com.thomas.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
